7b3b1b46807e7a0ba3ad851e4df6daee178c4839
anviiiijain <anvijain250102@gmail.com>
2023-02-16 06:47:44 +0530

    added basic animations

--- src/components/articles.tsx
+++ src/components/articles.tsx
@@ -1,93 +1,145 @@
 import { useState } from 'react'
 import ArticleCard, { ArticleCardProps } from './article-card'
 import cx from 'classnames'
-import Img1 from '../assets/article-img-1.png'
-import Img2 from '../assets/article-img-2.png'
-import Img3 from '../assets/article-img-3.png'
-import Img4 from '../assets/article-img-4.png'
-import Img5 from '../assets/article-img-5.png'
-import Img6 from '../assets/article-img-6.png'
 import { MdOutlineNavigateNext, MdOutlineNavigateBefore } from 'react-icons/md'
 import { Icon } from '@chakra-ui/react'
-import { AnimatePresence, motion } from 'framer-motion'
+import { AnimatePresence, motion, useAnimation } from 'framer-motion'
+import { articlesData } from '../utils/articlesData'
 
-interface ArticlesProps {}
+const variants = {
+  enter: (direction: number) => {
+    return {
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+      transition: {
+        x: { type: 'spring', stiffness: 300, damping: 30 },
+        opacity: { duration: 0.2 },
+      },
+    }
+  },
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+    transition: {
+      x: { type: 'spring', stiffness: 300, damping: 30 },
+      opacity: { duration: 0.2 },
+    },
+  },
+  exit: (direction: number) => {
+    return {
+      zIndex: 0,
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+    }
+  },
+}
 
-const articlesData = [
-  {
-    image: Img1,
-    heading: 'Grilled  Tomatoes at Home',
-    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+const slideUpVariant = {
+  hidden: { opacity: 0, y: 100 },
+  visible: { opacity: 1, y: 0 },
+  enter: (direction: number) => {
+    return {
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+      transition: {
+        x: { type: 'spring', stiffness: 300, damping: 30 },
+        opacity: { duration: 0.2 },
+      },
+    }
   },
-  {
-    image: Img2,
-    heading: 'Snacks for Travel',
-    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+    transition: {
+      x: { type: 'spring', stiffness: 300, damping: 30 },
+      opacity: { duration: 0.2 },
+    },
   },
-  {
-    image: Img3,
-    heading: 'Post-workout Recipes',
-    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
-  },
-  {
-    image: Img4,
-    heading: 'How To Grill Corn',
-    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
-  },
-  {
-    image: Img5,
-    heading: 'Crunchwrap Supreme',
-    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
-  },
-  {
-    image: Img6,
-    heading: 'Broccoli Cheese Soup',
-    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
-  },
-]
+}
 
-const Articles = (props: ArticlesProps) => {
-  const [group, setGroup] = useState(1)
+const Articles = () => {
+  const controls = useAnimation()
+  const [[page, direction], setPage] = useState([1, -1])
   const handleNext = () => {
-    if (group < 2) setGroup(2)
+    paginate(1)
   }
   const handlePrev = () => {
-    if (group > 1) setGroup(1)
+    paginate(-1)
   }
+
+  const paginate = (newDirection: number) => {
+    setPage([newDirection > 0 ? 1 + newDirection : 1, newDirection])
+    controls.set(variants.enter)
+    controls.start(variants.center)
+  }
+
   return (
     <div className='w-5/6 mx-auto mt-36'>
-      <h2 className='font-poppins md:font-source-sans-pro text-primary text-center lg:text-left text-[28px] md:text-4xl xl:text-[56px] font-semibold tracking-wide lg:tracking-widest xl:tracking-[0.04em]'>
+      <motion.h2
+        variants={slideUpVariant}
+        initial='hidden'
+        whileInView='visible'
+        transition={{ delay: 0.1, duration: 0.3 }}
+        viewport={{ once: true }}
+        className='font-poppins md:font-source-sans-pro text-primary text-center lg:text-left text-[28px] md:text-4xl xl:text-[56px] font-semibold tracking-wide lg:tracking-widest xl:tracking-[0.04em]'
+      >
         Latest Articles
-      </h2>
+      </motion.h2>
       <AnimatePresence>
         <div className='w-5/6 sm:w-auto mx-auto my-8 lg:mt-[5.5rem]'>
-          {/* first group */}
+          {/* first page */}
           <motion.div
-            animate={{ x: 0 }}
-            initial={{ x: '-100vw' }}
-            transition={{ type: 'tween' }}
+            custom={direction}
+            variants={variants}
+            animate={controls}
+            exit='exit'
             className={cx(
-              { hidden: group !== 1 },
+              { hidden: page !== 1 },
               'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-5 xl:gap-x-10'
             )}
           >
             {articlesData
-              ?.slice(0, articlesData.length / 2)
-              .map((item: ArticleCardProps) => (
-                <ArticleCard {...item} />
+              ?.slice(0, articlesData?.length / 2)
+              .map((item: ArticleCardProps, idx) => (
+                <motion.div
+                  variants={slideUpVariant}
+                  custom={direction}
+                  initial='hidden'
+                  whileInView='visible'
+                  animate={controls}
+                  viewport={{ once: true }}
+                  transition={{ delay: 0.1 + idx / 10, duration: 0.3 }}
+                >
+                  <ArticleCard {...item} />
+                </motion.div>
               ))}
           </motion.div>
-          {/* second group */}
+          {/* second page */}
           <motion.div
+            custom={direction}
+            variants={variants}
+            animate={controls}
             className={cx(
-              { hidden: group !== 2 },
+              { hidden: page !== 2 },
               'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-5 xl:gap-x-10'
             )}
           >
             {articlesData
-              ?.slice(articlesData.length / 2)
-              .map((item: ArticleCardProps) => (
-                <ArticleCard {...item} />
+              ?.slice(articlesData?.length / 2)
+              .map((item: ArticleCardProps, idx) => (
+                <motion.div
+                  variants={slideUpVariant}
+                  custom={direction}
+                  initial='hidden'
+                  whileInView='visible'
+                  animate={controls}
+                  viewport={{ once: true }}
+                  transition={{ delay: 0.1 + idx / 10, duration: 0.3 }}
+                >
+                  <ArticleCard {...item} />
+                </motion.div>
               ))}
           </motion.div>
         </div>
@@ -97,30 +149,30 @@ const Articles = (props: ArticlesProps) => {
           <Icon
             w={[4, 5, 6]}
             h={[4, 5, 6]}
-            color={group === 1 ? 'gray.light' : 'gray.dark'}
+            color={page === 1 ? 'gray.light' : 'gray.dark'}
             as={MdOutlineNavigateBefore}
             className={cx(
               {
-                'border-gray-light cursor-not-allowed': group === 1,
-                'border-gray-dark cursor-pointer': group === 2,
+                'border-gray-light cursor-not-allowed': page === 1,
+                'border-gray-dark cursor-pointer': page === 2,
               },
               ' border-[1px] rounded'
             )}
           />
         </span>
         <span className='text-xs md:text-sm lg:text-base xl:text-[22px] text-gray-dark'>
-          {group}/2
+          {page}/2
         </span>
         <span onClick={handleNext}>
           <Icon
             w={[4, 5, 6]}
             h={[4, 5, 6]}
-            color={group === 2 ? 'gray.light' : 'gray.dark'}
+            color={page === 2 ? 'gray.light' : 'gray.dark'}
             as={MdOutlineNavigateNext}
             className={cx(
               {
-                'border-gray-light cursor-not-allowed': group === 2,
-                'border-gray-dark cursor-pointer': group === 1,
+                'border-gray-light cursor-not-allowed': page === 2,
+                'border-gray-dark cursor-pointer': page === 1,
               },
               ' border-[1px] rounded'
             )}
---
+++ src/utils/articlesData.ts
@@ -0,0 +1,39 @@
+import Img1 from '../assets/article-img-1.png'
+import Img2 from '../assets/article-img-2.png'
+import Img3 from '../assets/article-img-3.png'
+import Img4 from '../assets/article-img-4.png'
+import Img5 from '../assets/article-img-5.png'
+import Img6 from '../assets/article-img-6.png'
+
+export const articlesData = [
+  {
+    image: Img1,
+    heading: 'Grilled  Tomatoes at Home',
+    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+  },
+  {
+    image: Img2,
+    heading: 'Snacks for Travel',
+    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+  },
+  {
+    image: Img3,
+    heading: 'Post-workout Recipes',
+    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+  },
+  {
+    image: Img4,
+    heading: 'How To Grill Corn',
+    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+  },
+  {
+    image: Img5,
+    heading: 'Crunchwrap Supreme',
+    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+  },
+  {
+    image: Img6,
+    heading: 'Broccoli Cheese Soup',
+    desc: "PLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard...",
+  },
+]
